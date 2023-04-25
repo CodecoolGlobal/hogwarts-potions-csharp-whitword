94755ef856012dc69d0750b0a88ba70645bf69f2
whitword <gyorgyi.lajterne@gmail.com>
2023-04-25 16:28:42 +0200

    Add PotionController

---
+++ Controllers/PotionController.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using HogwartsPotions.Models.Entities;
+using HogwartsPotions.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HogwartsPotions.Controllers;
+
+[ApiController, Route("/potions")]
+public class PotionController : ControllerBase
+{
+    private readonly IPotionService _potionService;
+
+    public PotionController(IPotionService potionService)
+    {
+        _potionService = potionService;
+    }
+    
+    [HttpGet]
+    public async Task<List<Potion>> GetAllPotions()
+    {
+        return await _potionService.GetAllPotions();
+    }
+}
